6d47c5bd31a54092fe2383e2162b53917ce05813
Otto Reimers <70694917+ottoreimers@users.noreply.github.com>
2021-03-31 11:29:03 +0200

    fixed some API stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from "react";
+import {Map, GoogleApiWrapper, Marker} from 'google-maps-react';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class Weather extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  state = {
+    loading: true,
+    temperature: null,
+    location: false,
+  }
+
+  async componentDidMount() {
+    navigator.geolocation.getCurrentPosition((data) => this.setState(
+      {location: data.coords})
+    );
+    const url = 'https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18/lat/59/data.json';
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({temperature: data.timeSeries[0], loading: false})
+  }
+
+
+
+  render() {
+
+    if (this.state.loading) {
+      return <div>loading...</div>
+    }
+
+    if (!this.state.temperature) {
+      return <div>Did not get data</div>
+    }
+
+    if (!this.state.location) {
+      return <div>Could not find you</div>
+    }
+
+    return <div>
+      <div>
+        <h1>{this.state.temperature.parameters[10].values} °C</h1>
+        <h4>Your location
+          {this.state.location.longitude} -
+          {this.state.location.latitude}</h4>
+
+          <h4>Wind {this.state.temperature.parameters[14].values} m/s</h4>
+      </div>
+    </div>;
+  }
 }
 
-export default App;
+
+
